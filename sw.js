8b86339d8c0fcc52eef3a66e74e892b9876ae841
aronkibetkorir93-ui <aronkibetkorir93@gmail.com>
2026-04-30 20:48:22 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const cacheName = 'teahub-v3';
+const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
